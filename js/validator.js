174370ca78943a25b8ee27b30d0fcec0dea10565
Ben Griffiths <mrbengriffiths@googlemail.com>
2012-04-26 23:27:59 +0100

    Added controller, gitignore initial feed list

---
+++ js/validator.js
@@ -0,0 +1,8 @@
+function ValidatorCtrl($scope) {
+    $scope.feeds = [
+            {desc: 'The verge RSS feed', uri: 'http://www.theverge.com/rss/full.xml'},
+            {desc: 'An invalid RSS feed', uri: 'http://www.theverge.com/rss/full.xml2'}
+    ];
+    $scope.passed = 0;
+    $scope.failed = 0;
+}
